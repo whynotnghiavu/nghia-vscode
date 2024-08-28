972743812f8a57f682949cec0986b273780a20fa
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-28 23:39:02 +0700

    update keybindings.py

--- contents/tools/keybindings.py
+++ contents/tools/keybindings.py
@@ -11,16 +11,22 @@ def get_keybindings_path():
         return os.path.expanduser('~/.config/Code/User/keybindings.json')
 
 
-nghia_keybindings_json = "../shortcuts/keybindings.json"
+nghia_keybindings_json = "../../input/shortcuts/keybindings.json"
 keybindings_path = get_keybindings_path()
 
 
-if os.path.exists(keybindings_path):
-    os.remove(keybindings_path)
+
+
+ 
+
+
 
 
 try:
+    if os.path.exists(keybindings_path):
+        os.remove(keybindings_path)
+        print(f"Đã xóa symlink: {keybindings_path}")
     os.symlink(os.path.abspath(nghia_keybindings_json), keybindings_path)
-    print(f"Symbolic link created at: {keybindings_path}")
+    print(f"Tạo link: {keybindings_path}")
 except OSError as e:
-    print(f"Failed to create symbolic link: {e}")
+    print(f"Lỗi: {e}")
